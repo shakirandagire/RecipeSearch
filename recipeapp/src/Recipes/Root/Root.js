65e0afd0602d2b45e793b240335c706b38fee27c
shakirandagire <ndagire.shakira@gmail.com>
2018-11-14 08:02:33 +0300

    Add functionality for search results

--- recipeapp/src/Recipes/Root/Root.js
+++ recipeapp/src/Recipes/Root/Root.js
@@ -1,15 +1,41 @@
 import React, {Component} from 'react';
 import { SearchBar } from '../SearchBar';
-import './Root.css';
+import { SearchResults } from '../SearchResults'
+import './Root.css'
+import mockData from '../mockData/mockData';
 
-export class Root extends Component {
+export class Root extends Component{
+    state = {
+        searchValue: '',
+        searchResults: []
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            searchValue: event.target.value
+            })
+        };
+
+    handleSearchClick = (event) => {
+        event.preventDefault();
+        const { searchValue } = this.state;
+        const data = mockData.filter(tem => tem.strCategory === searchValue);
+        this.setState({
+            searchResults: data
+            })
+        };
+
     render(){
-        return(
-            <div>
-                <h2>Search Recipes</h2>
-                <SearchBar/>
-            </div>
+        const searchResults = this.state;
+        return( 
+        <div>
+            <h2>Search Recipes</h2>
+            <SearchBar handleSearchClick={this.handleSearchClick} handleChange={this.handleChange} />
+            <SearchResults data={searchResults} />
+        </div>
 
         );
     }
+    
 }
+    
